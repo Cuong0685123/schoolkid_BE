ca426a647f750fabf7b24548c63495198db3eccf
Cuong0685123 <eticon0685@gmail.com>
2026-06-23 01:16:40 +0700

    Add SSL for Aiven MySQL

--- config/database.js
+++ config/database.js
@@ -10,9 +10,16 @@ export const sequelize = new Sequelize(
   process.env.DB_PASS,
   {
     host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT),
     dialect: "mysql",
     dialectModule: mysql2,
-    port: Number(process.env.DB_PORT) || 3306,
     logging: false,
+
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
   }
 );
