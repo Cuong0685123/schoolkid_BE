b4c6579a2a3dd600cfa20816f299532fe7ef0584
Cuong0685123 <eticon0685@gmail.com>
2026-06-22 23:56:14 +0700

    fix mysql2

--- config/database.js
+++ config/database.js
@@ -1,18 +1,21 @@
 import { Sequelize } from "sequelize";
+import mysql2 from "mysql2";
 import dotenv from "dotenv";
+
 dotenv.config({
-  path: process.env.NODE_ENV === 'production'
-    ? '.env.production'
-    : '.env.local'
-})
+  path: process.env.NODE_ENV === "production"
+    ? ".env.production"
+    : ".env.local"
+});
 
 export const sequelize = new Sequelize(
-  process.env.DB_NAME,     // database
-  process.env.DB_USER,     // user
-  process.env.DB_PASS,     // password
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASS,
   {
     host: process.env.DB_HOST,
     dialect: "mysql",
+    dialectModule: mysql2,
     port: process.env.DB_PORT || 3306,
     logging: false,
   }
